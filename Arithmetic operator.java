fe756892d007f379dd5c74d5abd40739b7f093b9
nandinimenda <160224691+nandinimenda@users.noreply.github.com>
2024-08-29 11:28:31 +0530

    Create Arithmetic operator.java

---
+++ Arithmetic operator.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Arithmetic
+{
+   public static void main(String[]args)
+   {
+      Scanner sc = new Scanner(System.in);
+      System.out.println("enter a,b values");
+      int a = sc.nextInt();
+      int b = sc.nextInt();
+      System.out.println(a+b);
+      System.out.println(a-b);
+      System.out.println(a*b);
+      System.out.println(a/b);
+      System.out.println(a%b);
+   }
+}
