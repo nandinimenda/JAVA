e155606a3e180b29ec785174e3cd3d8ba02a6de4
nandinimenda <160224691+nandinimenda@users.noreply.github.com>
2024-08-29 10:59:06 +0530

    Create default values.java

---
+++ default values.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Default
+{
+  static byte b;
+  static boolean bool;
+  static char ch;
+  static short s;
+  static int i;
+  static float f;
+  static long l;
+  static double d;
+  public static void main(String[]args)
+  {
+     System.out.println("default value of byte is "+b);
+     System.out.println("default value of byte is "+bool);
+     System.out.println("default value of char is "+ch);
+     System.out.println("default value of short is "+s);
+     System.out.println("default value of int is "+i);
+     System.out.println("default value of float is "+f);
+     System.out.println("default value of long is "+l);
+     System.out.println("default value of double is "+d);
+  }
+}
